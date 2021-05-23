eb4671b4f6e82ae7c427c87cf9072914a9526924
Евгений Клюев <craftycoderpro@gmail.com>
2021-05-23 19:07:29 +0300

    add pop-up(modal)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,71 +1,52 @@
 "use strict"
 
 import Swiper from 'swiper/bundle';
+import {
+  popUpSliderOptions
+} from './popUp/slider.js';
 
+const swiper = new Swiper('.swiper-container', popUpSliderOptions);
 
-
-
-
-
-
-const options = {
-  slidesPerView: 3,
-  direction: 'horizontal',
-  speed: 400,
-  spaceBetween: 100,
-
+//////////////////////////////////////////////////////////////////////////////
+const options1 = {
+  effect: 'fade',
+  slidesPerView: 2,
+  loop: false,
+  speed: 300,
+  enabled: false,
+  allowTouchMove: false, // скорее всего можно добавить через breakpoints - для телефона
+  zoom: {
+    maxRatio: 2,
+  },
   pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
+    el: ".swiper-miniatures",
+    clickable: true,
+    renderBullet: function(index, className) {
+      return `<img class=${className} src="./assets/images/sliderPopup/slide${index+1}.jpg" alt="tumbnail">`;
 
-    renderFraction: function(currentClass, totalClass) {
-      return '<span class="' + currentClass + '"></span>' +
-        ' - ' +
-        '<span class="' + totalClass + '"></span>';
     },
   },
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    dragSize: 200,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    },
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+const swiper1 = new Swiper('.swiper', options1);
+
+const popUpSliderWrapper = document.querySelector('.popUp__slider');
+const sliderItem = document.querySelector('.swiper-wrapper');
+const openPopUp = function(e) {
+  e.preventDefault();
+  popUpSliderWrapper.classList.toggle('popUp_open');
+
+  if (popUpSliderWrapper.classList.contains('popUp_open')) {
+    document.body.style.overflow = 'hidden';
+    swiper1.enable();
+    swiper1.update();
+    return;
   }
+
+  swiper1.disable()
+  document.body.style.overflow = '';
 }
-
-const swiper = new Swiper('.swiper-container', options);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let test = document.querySelector('#test');
-test.addEventListener('click', show);
-
-let wrapper = document.querySelector('.swiper-wrapper');
-
-function show() {
-
-  this.classList.toggle('popUpActive');
-  wrapper.classList.toggle('popUp');
-}
+document.querySelector('.popUp__openLink').addEventListener('click', openPopUp);
