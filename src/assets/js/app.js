07ce449bdc7440dfd0a2db24c3fcf1c57bf28513
Евгений Клюев <craftycoderpro@gmail.com>
2021-05-25 13:57:59 +0300

    add accordeon and simple bar(custom scrollbar)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,29 +5,44 @@ import {
   popUpSliderOptions
 } from './popUp/slider.js';
 
+import menu from './menu';
+menu();
+
+import SimpleBar from 'simplebar';
+
+const simpleBar = new SimpleBar(document.getElementById('headerMenu'), {
+  autoHide: false
+});
+
+
 const swiper = new Swiper('.swiper-container', popUpSliderOptions);
+
+
+
+
 
 //////////////////////////////////////////////////////////////////////////////
 const options1 = {
-  effect: 'fade',
-  slidesPerView: 2,
-  loop: false,
-  speed: 300,
+  effect: 'cube',
+  speed: 400,
   enabled: false,
   allowTouchMove: false, // скорее всего можно добавить через breakpoints - для телефона
   zoom: {
     maxRatio: 2,
   },
   pagination: {
-    el: ".swiper-miniatures",
+    el: ".swiper-tumbnails",
     clickable: true,
     renderBullet: function(index, className) {
       return `<img class=${className} src="./assets/images/sliderPopup/slide${index+1}.jpg" alt="tumbnail">`;
-
     },
   },
 }
 
+
+// document.querySelector('.swiper-container').addEventListener('dragstart', (e) => {
+//   e.preventDefault()
+// })
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -50,3 +65,107 @@ const openPopUp = function(e) {
   document.body.style.overflow = '';
 }
 document.querySelector('.popUp__openLink').addEventListener('click', openPopUp);
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// swiper.isEnd
+// swiper.slideNext(speed, runCallbacks)
+// allowTouchMove: false // можно свайпать только по slidePrev or slideNext
+
+
+// const menuButton = document.querySelector('.menu-button');
+
+// const onMenu = () => {
+//   swiper.enable();
+
+//   console.log(swiper.isBeginning, swiper.isEnd);
+
+//   if (swiper.isBeginning) {
+//     swiper.slideNext();
+//   } else {
+//     swiper.slidePrev();
+//   }
+// };
+
+// var swiper = new Swiper('#menuSlider', {
+//   slidesPerView: 'auto',
+//   initialSlide: 0,
+//   resistanceRatio: 0.8,
+//   enabled: false,
+//   slideToClickedSlide: true,
+
+//   on: {
+//     beforeInit: function() {
+//       menuButton.addEventListener('click', onMenu, true);
+//     },
+//     transitionEnd: function() {
+//       if (swiper.isEnd) {
+//         console.log('end')
+//         menuButton.classList.add('closebtnn');
+//       }
+
+//       if (swiper.isBeginning) {
+//         console.log("beginning");
+//         menuButton.classList.remove('closebtnn');
+//         swiper.disable();
+//       }
+
+
+//     },
+//   },
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const accorderon = document.querySelector('#accordeon');
+
+const accordeonItems = accordeon.children;
+
+// console.log(accordeonItems);
+accordeonItems[0].classList.add('open');
+
+const toggleItems = (e) => {
+  const target = e.target.closest('li');
+
+  for (let item of accordeonItems) {
+    (item === target) ? item.classList.add('open'): item.classList.remove('open')
+  }
+}
+
+
+accordeonItems.forEach(item => item.addEventListener('click', toggleItems))
